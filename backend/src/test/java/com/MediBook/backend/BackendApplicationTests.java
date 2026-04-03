e84067b9cd49ddfe3eeb8f1dda180f573b5ceeb4
ashfaq-ui <rizvimahamed456@gmail.com>
2026-04-03 11:25:15 +0530

    initialized the projects

---
+++ backend/src/test/java/com/MediBook/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MediBook.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
