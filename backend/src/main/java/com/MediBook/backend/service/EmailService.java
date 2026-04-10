6eb81f425e645e69c83992448eb1debf8159ce45
ashfaq-ui <rizvimahamed456@gmail.com>
2026-04-10 23:18:07 +0530

    backend

--- backend/src/main/java/com/MediBook/backend/service/EmailService.java
+++ backend/src/main/java/com/MediBook/backend/service/EmailService.java
@@ -15,7 +15,7 @@ public class EmailService {
 
     private final JavaMailSender mailSender;
 
-    @Value("${spring.mail.username}")
+    @Value("${mail.from:onboarding@resend.dev}")
     private String fromEmail;
 
     @Async
