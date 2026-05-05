d6c6bc18d6ad6fcb4d78b7ffcf71c6bed31b60b9
ashfaq-ui <rizvimahamed456@gmail.com>
2026-05-05 12:45:49 +0530

    test case bug resolved-2

--- backend/src/test/java/com/MediBook/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/MediBook/backend/BackendApplicationTests.java
@@ -1,4 +1,8 @@
+package com.MediBook.backend;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
 @TestPropertySource(properties = {
